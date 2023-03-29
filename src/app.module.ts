27523f6e8a2d03257a36329edd39dec08c1f4d30
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-03-29 17:34:20 -0300

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, 'mongodb+srv://Bruno:<password>@databases.i735gb3.mongodb.net/?retryWrites=true&w=majority'],
   controllers: [AppController],
   providers: [AppService],
 })
