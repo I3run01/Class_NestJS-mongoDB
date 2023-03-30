8faac1249bad0558cacbd3084fd4d0627318ef27
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-03-29 21:54:33 -0300

    c

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,23 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcrypt'
+import * as base64 from 'base64-js';
+
+let  reqresUserRequest = async (id:string) => {  
+  let response = await fetch(`https://reqres.in/api/users/${id}`)
+  let json = await response.json()
+
+  return json.data
+}
+
+async function imageToBase64(url: string): Promise<string> {
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  const bytes = new Uint8Array(buffer);
+  const encodedImage = base64.fromByteArray(bytes);
+  return encodedImage;
+}
 
 @Controller('api/users')
 export class UsersController {
@@ -12,13 +29,31 @@ export class UsersController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return await this.usersService.findOne(id);
+  async findUser(@Param('id') id: string) {
+    return await reqresUserRequest(id);
   }
 
   @Get(':id/avatar')
   async saveImage(@Param('id') id: string) {
-    return await this.usersService.saveImage(id);
+    let userInDatabase = await this.usersService.findOne(id);
+
+    if(userInDatabase) {
+      return {token: userInDatabase.token}
+    }
+
+    let reqreUser = await reqresUserRequest(id)
+
+    let createUserDto: CreateUserDto = {
+      email: reqreUser.email,
+      firstName: reqreUser.firstName,
+      id: id,
+      imageInBase64: await imageToBase64(reqreUser.avatar),
+      lastName: reqreUser.lastName,
+      password: '',
+      token: await hash(reqreUser.email, 10),
+    }
+    return this.usersService.create(createUserDto);
+
   }
 
   @Delete(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,52 +4,25 @@ import { User, UserDocument } from './entities/user.entity';
 import { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose'
 import { hash } from 'bcrypt'
-import * as base64 from 'base64-js';
 
-async function imageToBase64(url: string): Promise<string> {
-  const response = await fetch(url);
-  const buffer = await response.arrayBuffer();
-  const bytes = new Uint8Array(buffer);
-  const encodedImage = base64.fromByteArray(bytes);
-  return encodedImage;
-}
+
+
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto) {
-    createUserDto.password = await hash('createUserDto.password', 10)
+    createUserDto.password = await hash(createUserDto.password, 10)
     return this.userModel.create(createUserDto);
   }
 
   async findOne(id: string) {
-    let response = await fetch(`https://reqres.in/api/users/${id}`)
-    let responseInJson = await response.json()
-    return await responseInJson.data
-  }
-
-  async saveImage(id: string) {
-
-    let hasUser = await this.userModel.findOne({id:id})
-    if(hasUser !== null) {
-      return {hasUser}
-      
+    let user = this.userModel.findOne({id:id})
+    if(user) {
+      return user
     }
-
-    let response = await fetch(`https://reqres.in/api/users/${id}`)
-    let responseInJson = await response.json()
-    let responseDatas = await responseInJson.data
-
-    let token =  String(await hash(responseDatas.email, 10))
-    let encoded64 = await imageToBase64(responseDatas.avatar)
-    console.log(encoded64)
-
-    return this.userModel.create({
-      token: token,
-      imageInBase64: encoded64,
-      id: responseDatas.id
-    });
+    return null
   }
 
   remove(id: string) {
