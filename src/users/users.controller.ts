f41e20a8be273cbe1545391f53737dce92c7d95a
I3run01 <brunnooa.v@gmail.com>
2023-03-31 15:08:05 -0300

    c

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,74 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { hash } from 'bcrypt';
-import * as complamentaryFunctions from '../Functions/functions';
-
+import * as utils from '../Functions/functions';
 
 @Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() userData:{email: string, password: string}) {
+  async create(@Body() userData: { email: string; password: string }) {
+    const hashedPassword = await hash(userData.password, 10);
+    const hashedEmail = await hash(userData.email, 10);
 
-    let userDTO: CreateUserDto = {
+    const userDTO: CreateUserDto = {
       taskOne: {
         email: userData.email,
-        password: await hash(userData.password, 10),
-        token: await hash(userData.email, 10),
+        password: hashedPassword,
+        token: hashedEmail,
       },
       taskThree: {
         id: null,
         imageRouter: null,
         hash: null,
-        imageCode64: null
-      }
-    }
+        imageCode64: null,
+      },
+    };
 
-    complamentaryFunctions.sendEmail('brunnooa.v@gmail.com','testEmail',"I'm junst testing")
-
+    utils.sendEmail('brunnooa.v@gmail.com', 'testEmail', "I'm just testing");
     return await this.usersService.create(userDTO);
   }
 
   @Get(':id')
   async findUser(@Param('id') id: string) {
-    return await complamentaryFunctions.reqresUserRequest(id);
+    return await utils.reqresUserRequest(id);
   }
 
   @Get(':id/avatar')
   async saveImage(@Param('id') id: string) {
     let userInDatabase = await this.usersService.findOne(id);
+    let imageRouter:string, imageEncode64:string | null;
 
-    if(userInDatabase) {
-      let imageEncode64 = userInDatabase.taskThree.imageCode64
-      return {encode64: imageEncode64}
-    }
+    if (userInDatabase) {
+      return userInDatabase.taskThree.imageCode64;
+    } 
+    
+    const reqreUser = await utils.reqresUserRequest(id);
+    imageRouter = `./uploads/images/img${reqreUser.id}.jpg`;
+    await utils.saveImageFromUrl(reqreUser.avatar, imageRouter);
 
-    let reqreUser = await complamentaryFunctions.reqresUserRequest(id)
-    let imageRouter = `./uploads/images/img${reqreUser.id}.jpg`;
-    await complamentaryFunctions.saveImageFromUrl(reqreUser.avatar, imageRouter)
+    imageEncode64 = await utils.encodeImageToBase64(imageRouter);
 
-    let imageEncode64 = await complamentaryFunctions.encodeImageToBase64(imageRouter)
-
-    let userDTO: CreateUserDto = {
+    const userDTO: CreateUserDto = {
       taskOne: {
         email: null,
         password: null,
-        token: null
+        token: null,
       },
       taskThree: {
         id: id,
         imageRouter: imageRouter,
         hash: String(await hash(reqreUser.email, 10)),
-        imageCode64: imageEncode64
-      }
-      
-    }
+        imageCode64: imageEncode64,
+      },
+    };
 
-    await this.usersService.create(userDTO)
-    return {encoded64: imageEncode64};
+    await this.usersService.create(userDTO);
+    
 
+    return { encoded64: imageEncode64 };
   }
 
   @Delete(':id')
@@ -76,15 +76,16 @@ export class UsersController {
     let userInDatabase = await this.usersService.findOne(id);
 
     if (!userInDatabase) {
-      complamentaryFunctions.deleteImageFromID(id);
+      utils.deleteImageFromID(id);
       return { status: 'User does not exist in database' };
-    } 
+    }
 
     let imageRouter = userInDatabase.taskThree.imageRouter;
+
     if (imageRouter) {
-      complamentaryFunctions.deleteImage(imageRouter);
+      utils.deleteImage(imageRouter);
     } else {
-      complamentaryFunctions.deleteImageFromID(id);
+      utils.deleteImageFromID(id);
     }
 
     return this.usersService.remove(id);
