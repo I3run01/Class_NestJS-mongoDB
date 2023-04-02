cc7ac95aa8548a6bc066078fc8c24835d3725f4a
I3run01 <brunnooa.v@gmail.com>
2023-04-02 14:38:13 -0300

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,6 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     UsersModule,
     MongooseModule.forRoot('mongodb+srv://Bruno:1515@databases.i735gb3.mongodb.net/payeverTest1?retryWrites=true&w=majority'),
-    ClientsModule.register([
-      {
-        name: 'MATH_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://localhost:5672'],
-          queue: 'cats_queue',
-          queueOptions: {
-            durable: false
-          },
-        },
-      },
-    ])
   ],
   controllers: [AppController],
   providers: [AppService],
