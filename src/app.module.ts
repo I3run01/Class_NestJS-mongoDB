f69a38bee2a5fd3ad17dd600b867455c414be844
I3run01 <brunnooa.v@gmail.com>
2023-04-03 14:44:48 -0300

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(process.env.mongoURL),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL as string),
   ],
   controllers: [AppController],
   providers: [AppService],
