64d4417810686f0fc81e3507816dba917d151c6e
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-06 19:31:08 -0300

    c

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -24,7 +24,7 @@ export class UsersService {
 
   async remove(id: string) {
       return await this.userModel.deleteOne({
-        'taskThree.id': 890,
+        'taskThree.id': id,
       });
   }
 
